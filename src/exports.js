d746c786b847470799711d5f034080ec036fe988
kai zhu <kaizhu256@gmail.com>
2020-02-26 00:12:24 -0600

    jslint

--- src/exports.js
+++ src/exports.js
@@ -1,3 +1,4 @@
+/* jslint utility2:true */
 var RegisterExtensionFunctions;
 var i;
 var sqlite3_bind_blob;
@@ -27,8 +28,8 @@ var sqlite3_reset;
 var sqlite3_result_blob;
 var sqlite3_result_double;
 var sqlite3_result_error;
-var sqlite3_result_int;
 var sqlite3_result_int64;
+var sqlite3_result_int;
 var sqlite3_result_null;
 var sqlite3_result_text;
 var sqlite3_step;
@@ -39,86 +40,86 @@ var sqlite3_value_int;
 var sqlite3_value_text;
 var sqlite3_value_type;
 
-sqlite3_open = Module.cwrap('sqlite3_open', 'number', ['string', 'number']);
+sqlite3_open = Module.cwrap("sqlite3_open", "number", ["string", "number"]);
 
-sqlite3_close_v2 = Module.cwrap('sqlite3_close_v2', 'number', ['number']);
+sqlite3_close_v2 = Module.cwrap("sqlite3_close_v2", "number", ["number"]);
 
-sqlite3_exec = Module.cwrap('sqlite3_exec', 'number', ['number', 'string', 'number', 'number', 'number']);
+sqlite3_exec = Module.cwrap("sqlite3_exec", "number", ["number", "string", "number", "number", "number"]);
 
-sqlite3_free = Module.cwrap('sqlite3_free', '', ['number']);
+sqlite3_free = Module.cwrap("sqlite3_free", "", ["number"]);
 
-sqlite3_changes = Module.cwrap('sqlite3_changes', 'number', ['number']);
+sqlite3_changes = Module.cwrap("sqlite3_changes", "number", ["number"]);
 
-sqlite3_prepare_v2 = Module.cwrap('sqlite3_prepare_v2', 'number', ['number', 'string', 'number', 'number', 'number']);
+sqlite3_prepare_v2 = Module.cwrap("sqlite3_prepare_v2", "number", ["number", "string", "number", "number", "number"]);
 
-sqlite3_prepare_v2_sqlptr = Module.cwrap('sqlite3_prepare_v2', 'number', ['number', 'number', 'number', 'number', 'number']);
+sqlite3_prepare_v2_sqlptr = Module.cwrap("sqlite3_prepare_v2", "number", ["number", "number", "number", "number", "number"]);
 
-sqlite3_bind_text = Module.cwrap('sqlite3_bind_text', 'number', ['number', 'number', 'number', 'number', 'number']);
+sqlite3_bind_text = Module.cwrap("sqlite3_bind_text", "number", ["number", "number", "number", "number", "number"]);
 
-sqlite3_bind_blob = Module.cwrap('sqlite3_bind_blob', 'number', ['number', 'number', 'number', 'number', 'number']);
+sqlite3_bind_blob = Module.cwrap("sqlite3_bind_blob", "number", ["number", "number", "number", "number", "number"]);
 
-sqlite3_bind_double = Module.cwrap('sqlite3_bind_double', 'number', ['number', 'number', 'number']);
+sqlite3_bind_double = Module.cwrap("sqlite3_bind_double", "number", ["number", "number", "number"]);
 
-sqlite3_bind_int = Module.cwrap('sqlite3_bind_int', 'number', ['number', 'number', 'number']);
+sqlite3_bind_int = Module.cwrap("sqlite3_bind_int", "number", ["number", "number", "number"]);
 
-sqlite3_bind_parameter_index = Module.cwrap('sqlite3_bind_parameter_index', 'number', ['number', 'string']);
+sqlite3_bind_parameter_index = Module.cwrap("sqlite3_bind_parameter_index", "number", ["number", "string"]);
 
-sqlite3_step = Module.cwrap('sqlite3_step', 'number', ['number']);
+sqlite3_step = Module.cwrap("sqlite3_step", "number", ["number"]);
 
-sqlite3_errmsg = Module.cwrap('sqlite3_errmsg', 'string', ['number']);
+sqlite3_errmsg = Module.cwrap("sqlite3_errmsg", "string", ["number"]);
 
-sqlite3_data_count = Module.cwrap('sqlite3_data_count', 'number', ['number']);
+sqlite3_data_count = Module.cwrap("sqlite3_data_count", "number", ["number"]);
 
-sqlite3_column_double = Module.cwrap('sqlite3_column_double', 'number', ['number', 'number']);
+sqlite3_column_double = Module.cwrap("sqlite3_column_double", "number", ["number", "number"]);
 
-sqlite3_column_text = Module.cwrap('sqlite3_column_text', 'string', ['number', 'number']);
+sqlite3_column_text = Module.cwrap("sqlite3_column_text", "string", ["number", "number"]);
 
-sqlite3_column_blob = Module.cwrap('sqlite3_column_blob', 'number', ['number', 'number']);
+sqlite3_column_blob = Module.cwrap("sqlite3_column_blob", "number", ["number", "number"]);
 
-sqlite3_column_bytes = Module.cwrap('sqlite3_column_bytes', 'number', ['number', 'number']);
+sqlite3_column_bytes = Module.cwrap("sqlite3_column_bytes", "number", ["number", "number"]);
 
-sqlite3_column_type = Module.cwrap('sqlite3_column_type', 'number', ['number', 'number']);
+sqlite3_column_type = Module.cwrap("sqlite3_column_type", "number", ["number", "number"]);
 
-sqlite3_column_name = Module.cwrap('sqlite3_column_name', 'string', ['number', 'number']);
+sqlite3_column_name = Module.cwrap("sqlite3_column_name", "string", ["number", "number"]);
 
-sqlite3_reset = Module.cwrap('sqlite3_reset', 'number', ['number']);
+sqlite3_reset = Module.cwrap("sqlite3_reset", "number", ["number"]);
 
-sqlite3_clear_bindings = Module.cwrap('sqlite3_clear_bindings', 'number', ['number']);
+sqlite3_clear_bindings = Module.cwrap("sqlite3_clear_bindings", "number", ["number"]);
 
-sqlite3_finalize = Module.cwrap('sqlite3_finalize', 'number', ['number']);
+sqlite3_finalize = Module.cwrap("sqlite3_finalize", "number", ["number"]);
 
-sqlite3_create_function_v2 = Module.cwrap('sqlite3_create_function_v2', 'number', ['number', 'string', 'number', 'number', 'number', 'number', 'number', 'number', 'number']);
+sqlite3_create_function_v2 = Module.cwrap("sqlite3_create_function_v2", "number", ["number", "string", "number", "number", "number", "number", "number", "number", "number"]);
 
-sqlite3_value_type = Module.cwrap('sqlite3_value_type', 'number', ['number']);
+sqlite3_value_type = Module.cwrap("sqlite3_value_type", "number", ["number"]);
 
-sqlite3_value_bytes = Module.cwrap('sqlite3_value_bytes', 'number', ['number']);
+sqlite3_value_bytes = Module.cwrap("sqlite3_value_bytes", "number", ["number"]);
 
-sqlite3_value_text = Module.cwrap('sqlite3_value_text', 'string', ['number']);
+sqlite3_value_text = Module.cwrap("sqlite3_value_text", "string", ["number"]);
 
-sqlite3_value_int = Module.cwrap('sqlite3_value_int', 'number', ['number']);
+sqlite3_value_int = Module.cwrap("sqlite3_value_int", "number", ["number"]);
 
-sqlite3_value_blob = Module.cwrap('sqlite3_value_blob', 'number', ['number']);
+sqlite3_value_blob = Module.cwrap("sqlite3_value_blob", "number", ["number"]);
 
-sqlite3_value_double = Module.cwrap('sqlite3_value_double', 'number', ['number']);
+sqlite3_value_double = Module.cwrap("sqlite3_value_double", "number", ["number"]);
 
-sqlite3_result_double = Module.cwrap('sqlite3_result_double', '', ['number', 'number']);
+sqlite3_result_double = Module.cwrap("sqlite3_result_double", "", ["number", "number"]);
 
-sqlite3_result_null = Module.cwrap('sqlite3_result_null', '', ['number']);
+sqlite3_result_null = Module.cwrap("sqlite3_result_null", "", ["number"]);
 
-sqlite3_result_text = Module.cwrap('sqlite3_result_text', '', ['number', 'string', 'number', 'number']);
+sqlite3_result_text = Module.cwrap("sqlite3_result_text", "", ["number", "string", "number", "number"]);
 
-sqlite3_result_blob = Module.cwrap('sqlite3_result_blob', '', ['number', 'number', 'number', 'number']);
+sqlite3_result_blob = Module.cwrap("sqlite3_result_blob", "", ["number", "number", "number", "number"]);
 
-sqlite3_result_int = Module.cwrap('sqlite3_result_int', '', ['number', 'number']);
+sqlite3_result_int = Module.cwrap("sqlite3_result_int", "", ["number", "number"]);
 
-sqlite3_result_int64 = Module.cwrap('sqlite3_result_int64', '', ['number', 'number']);
+sqlite3_result_int64 = Module.cwrap("sqlite3_result_int64", "", ["number", "number"]);
 
-sqlite3_result_error = Module.cwrap('sqlite3_result_error', '', ['number', 'string', 'number']);
+sqlite3_result_error = Module.cwrap("sqlite3_result_error", "", ["number", "string", "number"]);
 
-RegisterExtensionFunctions = Module.cwrap('RegisterExtensionFunctions', 'number', ['number']);
+RegisterExtensionFunctions = Module.cwrap("RegisterExtensionFunctions", "number", ["number"]);
 
 this.SQL = {
-    'Database': Database
+    "Database": Database
 };
 
 for (i in this.SQL) {
