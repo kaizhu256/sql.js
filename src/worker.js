f33dbd2b5942c1193978b2604ecf39dc5727b1d4
kai zhu <kaizhu256@gmail.com>
2020-02-26 00:49:59 -0600

    jslint

--- src/worker.js
+++ src/worker.js
@@ -1,4 +1,5 @@
 /* jslint utility2:true */
+/*global SQL importScripts intSqlJs self*/
 // Since this is only included in web worker builds,
 // I'm not sure if we need to detect this.
 // In fact, it seems like we might want to throw an error
@@ -10,7 +11,7 @@ var sqlModuleReady;
 if (typeof importScripts === "function") {
     db = null;
     createDb = function (data) {
-        if (db != null) {
+        if (db !== null) {
             db.close();
         }
         return db = new SQL.Database(data);
@@ -24,25 +25,25 @@ if (typeof importScripts === "function") {
             var done;
             var err;
             var result;
-            data = event["data"];
-            switch (data != null ? data["action"] : void 0) {
+            data = event.data;
+            switch (data !== null ? data.action : void 0) {
             case "open":
-                buff = data["buffer"];
+                buff = data.buffer;
                 createDb((buff ? new Uint8Array(buff) : void 0));
                 return postMessage({
-                    "id": data["id"],
+                    "id": data.id,
                     "ready": true
                 });
             case "exec":
                 if (db === null) {
                     createDb();
                 }
-                if (!data["sql"]) {
+                if (!data.sql) {
                     throw "exec: Missing query string";
                 }
                 return postMessage({
-                    "id": data["id"],
-                    "results": db.exec(data["sql"])
+                    "id": data.id,
+                    "results": db.exec(data.sql)
                 });
             case "each":
                 if (db === null) {
@@ -50,22 +51,22 @@ if (typeof importScripts === "function") {
                 }
                 callback = function (row) {
                     return postMessage({
-                        "id": data["id"],
+                        "id": data.id,
                         "row": row,
                         "finished": false
                     });
                 };
                 done = function () {
                     return postMessage({
-                        "id": data["id"],
+                        "id": data.id,
                         "finished": true
                     });
                 };
-                return db.each(data["sql"], data["params"], callback, done);
+                return db.each(data.sql, data.params, callback, done);
             case "export":
                 buff = db.export();
                 result = {
-                    "id": data["id"],
+                    "id": data.id,
                     "buffer": buff
                 };
                 try {
@@ -78,14 +79,14 @@ if (typeof importScripts === "function") {
                 }
                     break;
             case "close":
-                return db != null ? db.close() : void 0;
+                return db !== null ? db.close() : void 0;
             default:
-                throw new Error("Invalid action : " + (data != null ? data["action"] : void 0));
+                throw new Error("Invalid action : " + (data !== null ? data.action : void 0));
             }
         })["catch"](function (err) {
             return postMessage({
-                "id": event["data"]["id"],
-                "error": err["message"]
+                "id": event.data.id,
+                "error": err.message
             });
         });
     };
